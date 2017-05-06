c72f31f5a2f2bcf2371b684913bba8fa4c7fd823
Mikeokslonger <hattrickeric@gmail.com>
2017-05-06 11:18:56 -0700

    Added more unit tests and cleaned up code

--- pagerank.py
+++ pagerank.py
@@ -1,81 +1,166 @@
 import networkx as nx
+import unittest
 
-edgetype_scale = {'sector': 0.001,
-                  'supplier': 0.2}
 
-def pagerank_edgetypes(D, alpha=0.85, personalization=None,
-                       max_iter=100, tol=1.0e-6, nstart=None, weight='weight',
-                       dangling=None):
-    # Create a copy in (right) stochastic form
+def pagerank_edgetypes(D, edgetype_scale, max_iter=100, tol=1.0e-6, weight='weight'):
     W = nx.stochastic_graph(D, weight=weight)
     N = W.number_of_nodes()
 
-    # Choose fixed starting vector if not given
     x = dict.fromkeys(W, 1.0 / N)
     p = dict.fromkeys(W, 1.0 / N)
-    dangling_weights = p
-    dangling_nodes = [n for n in W if W.out_degree(n, weight=weight) == 0.0]
 
-    # power iteration: make up to max_iter iterations
     for _ in range(max_iter):
         xlast = x
         x = dict.fromkeys(xlast.keys(), 0)
         weight_to_distribute = sum([(xlast[n] * W[n][nbr]['weight'] * edgetype_scale[W[n][nbr]['type']]) for n in x for nbr in W[n]])
         undistributed_weight = 1 - weight_to_distribute
         for n in x:
-            # this matrix multiply looks odd because it is
-            # doing a left multiply x^T=xlast^T*W
             for nbr in W[n]:
                 x[nbr] += xlast[n] * W[n][nbr][weight] * edgetype_scale[W[n][nbr]['type']]
-            x[n] += undistributed_weight * dangling_weights.get(n, 0)
-        # check convergence, l1 norm
+            x[n] += undistributed_weight * p.get(n, 0)
+
         err = sum([abs(x[n] - xlast[n]) for n in x])
         if err < N * tol:
             return x
     raise nx.PowerIterationFailedConvergence(max_iter)
 
 
-def test_same_relation_types():
-    g = nx.DiGraph()
-    g.add_edges_from([('A', 'B', {'type': 'supplier'}),
-                      ('A', 'C', {'type': 'supplier'})])
-    r = pagerank_edgetypes(g, alpha=1)
-    assert ([k for k, v in sorted(r.iteritems(), key=lambda (k, v): v, reverse=True)] == ['C', 'B', 'A'])
+class EdgeTypePageRankTest(unittest.TestCase):
+
+    edgetype_scale = {'area': 0.001,
+                      'friend': 0.2}
+
+    def test_same_relation_types(self):
+        g = nx.DiGraph()
+        g.add_edges_from([('A', 'B', {'type': 'friend'}),
+                          ('A', 'C', {'type': 'friend'})])
+        r = pagerank_edgetypes(g, EdgeTypePageRankTest.edgetype_scale)
+        assert ([k for k, v in sorted(r.iteritems(), key=lambda (k, v): v, reverse=True)] == ['C', 'B', 'A'])
+
+    def test_same_relation_types_normalized_node_weights(self):
+        g = nx.DiGraph()
+        g.add_edges_from([('A', 'B', {'weight': 1.0, 'type': 'friend'}),
+                          ('A', 'C', {'weight': 0.5, 'type': 'friend'})])
+        r = pagerank_edgetypes(g, EdgeTypePageRankTest.edgetype_scale)
+        assert ([k for k, v in sorted(r.iteritems(), key=lambda (k, v): v, reverse=True)] == ['B', 'C', 'A'])
+
+    def test_multiple_relation_types(self):
+        g = nx.DiGraph()
+        g.add_edges_from([('A', 'B', {'type': 'friend'}),
+                          ('A', 'B', {'type': 'area'}),
+                          ('A', 'C', {'type': 'friend'})])
+        r = pagerank_edgetypes(g, EdgeTypePageRankTest.edgetype_scale)
+        assert ([k for k, v in sorted(r.iteritems(), key=lambda (k, v): v, reverse=True)] == ['C', 'B', 'A'])
+
+    # Would expect the same order as in test_same_relation_types but it's off as
+    # B and C are different on the 16th decimal place
+    def test_multiple_relation_types_circular(self):
+        g = nx.DiGraph()
+        g.add_edges_from([('A', 'B', {'type': 'area'}),
+                          ('B', 'A', {'type': 'area'}),
+                          ('A', 'C', {'type': 'area'}),
+                          ('C', 'A', {'type': 'area'}),
+                          ('B', 'C', {'type': 'area'}),
+                          ('C', 'B', {'type': 'area'}),
+                          ('A', 'B', {'type': 'friend'}),
+                          ('A', 'C', {'type': 'friend'})])
+        r = pagerank_edgetypes(g, EdgeTypePageRankTest.edgetype_scale)
+        assert ([k for k, v in sorted(r.iteritems(), key=lambda (k, v): v, reverse=True)] == ['B', 'C', 'A'])
+
+    def test_multiple_relation_types_circular(self):
+        g = nx.DiGraph()
+        g.add_edges_from([('A', 'B', {'type': 'area'}),
+                          ('B', 'A', {'type': 'area'}),
+                          ('A', 'C', {'type': 'area'}),
+                          ('C', 'A', {'type': 'area'}),
+                          ('B', 'C', {'type': 'area'}),
+                          ('C', 'B', {'type': 'area'}),
+                          ('A', 'B', {'type': 'friend'}),
+                          ('A', 'C', {'type': 'friend'})])
+        r = pagerank_edgetypes(g, EdgeTypePageRankTest.edgetype_scale)
+        assert ([k for k, v in sorted(r.iteritems(), key=lambda (k, v): v, reverse=True)] == ['B', 'C', 'A'])
 
 
-def test_same_relation_types_normalized_node_weights():
-    g = nx.DiGraph()
-    g.add_edges_from([('A', 'B', {'weight': 1.0, 'type': 'supplier'}),
-                      ('A', 'C', {'weight': 0.5, 'type': 'supplier'})])
-    r = pagerank_edgetypes(g, alpha=1)
-    assert ([k for k, v in sorted(r.iteritems(), key=lambda (k, v): v, reverse=True)] == ['B', 'C', 'A'])
+    def test_multiple_relation_types_circular_relative_edgetype_weights(self):
+        g = nx.DiGraph()
+        edges = [('A', 'B', {'type': 'area'}),
+                 ('B', 'A', {'type': 'area'}),
+                 ('A', 'C', {'type': 'area'}),
+                 ('C', 'A', {'type': 'area'}),
+                 ('B', 'C', {'type': 'area'}),
+                 ('C', 'B', {'type': 'area'}),
+                 ('A', 'B', {'type': 'friend'}),
+                 ('A', 'C', {'type': 'friend'})]
+        #edgetype_scale = {from_node, to_node, attr for }
+        edge_types = set([attr['type'] for (_, _, attr) in edges])
+        edge_type_scale = {t: 1.0 / len([e for e in edges if e[2]['type'] == t]) for t in edge_types}
+        g.add_edges_from(edges)
+        r = pagerank_edgetypes(g, edge_type_scale)
+        assert ([k for k, v in sorted(r.iteritems(), key=lambda (k, v): v, reverse=True)] == ['C', 'B', 'A'])
 
+    def test_multiple_relation_types_circular_multiple_areas(self):
+        g = nx.DiGraph()
+        edges = [('A', 'B', {'type': 'area'}),
+                 ('B', 'A', {'type': 'area'}),
+                 ('A', 'E', {'type': 'area'}),
+                 ('E', 'A', {'type': 'area'}),
+                 ('B', 'E', {'type': 'area'}),
+                 ('E', 'B', {'type': 'area'}),
+                 ('C', 'D', {'type': 'area'}),
+                 ('D', 'C', {'type': 'area'}),
+                 ('B', 'C', {'type': 'friend'}),
+                 ('B', 'A', {'type': 'friend'})]
+        #edgetype_scale = {from_node, to_node, attr for }
+        edge_types = set([attr['type'] for (_, _, attr) in edges])
+        edge_type_scale = {t: 1.0 / len([e for e in edges if e[2]['type'] == t]) for t in edge_types}
+        g.add_edges_from(edges)
+        r = pagerank_edgetypes(g, edge_type_scale)
+        assert ([k for k, v in sorted(r.iteritems(), key=lambda (k, v): v, reverse=True)] == ['C', 'A', 'D', 'B', 'E'])
 
-def test_multiple_relation_types():
-    g = nx.DiGraph()
-    g.add_edges_from([('A', 'B', {'type': 'supplier'}),
-                      ('A', 'B', {'type': 'sector'}),
-                      ('A', 'C', {'type': 'supplier'})])
-    r = pagerank_edgetypes(g, alpha=1)
-    assert ([k for k, v in sorted(r.iteritems(), key=lambda (k, v): v, reverse=True)] == ['C', 'B', 'A'])
+    def test_multiple_relation_types_circular_multiple_areas_big(self):
+        g = nx.DiGraph()
+        edges = [('A', 'B', {'type': 'area'}),
+                 ('B', 'A', {'type': 'area'}),
+                 ('A', 'C', {'type': 'area'}),
+                 ('C', 'A', {'type': 'area'}),
+                 ('A', 'D', {'type': 'area'}),
+                 ('D', 'A', {'type': 'area'}),
+                 ('A', 'J', {'type': 'area'}),
+                 ('J', 'A', {'type': 'area'}),
+                 ('B', 'C', {'type': 'area'}),
+                 ('C', 'B', {'type': 'area'}),
+                 ('B', 'D', {'type': 'area'}),
+                 ('D', 'B', {'type': 'area'}),
+                 ('B', 'J', {'type': 'area'}),
+                 ('J', 'B', {'type': 'area'}),
+                 ('C', 'D', {'type': 'area'}),
+                 ('D', 'C', {'type': 'area'}),
+                 ('C', 'J', {'type': 'area'}),
+                 ('J', 'C', {'type': 'area'}),
+                 ('D', 'J', {'type': 'area'}),
+                 ('J', 'D', {'type': 'area'}),
+                 ('E', 'F', {'type': 'area'}),
+                 ('F', 'E', {'type': 'area'}),
+                 ('E', 'G', {'type': 'area'}),
+                 ('G', 'E', {'type': 'area'}),
+                 ('F', 'G', {'type': 'area'}),
+                 ('G', 'F', {'type': 'area'}),
+                 ('H', 'I', {'type': 'area'}),
+                 ('I', 'H', {'type': 'area'}),
+                 ('B', 'A', {'type': 'friend'}),
+                 ('B', 'C', {'type': 'friend'}),
+                 ('C', 'E', {'type': 'foe'}),
+                 ('E', 'B', {'type': 'foe'}),
+                 ('D', 'F', {'type': 'friend'}),
+                 ('F', 'I', {'type': 'owner'}),
+                 ('F', 'H', {'type': 'owner'}),
+                 ('G', 'D', {'type': 'rival'}),
+                 ]
+        # edgetype_scale = {from_node, to_node, attr for }
+        edge_types = set([attr['type'] for (_, _, attr) in edges])
+        edge_type_scale = {t: 1.0 / len([e for e in edges if e[2]['type'] == t]) for t in edge_types}
+        g.add_edges_from(edges)
+        r = pagerank_edgetypes(g, edge_type_scale)
+        assert ([k for k, v in sorted(r.iteritems(), key=lambda (k, v): v, reverse=True)] ==
+                ['D', 'B', 'I', 'H', 'E', 'C', 'A', 'F', 'J', 'G'])
 
-
-# Would expect the same order as in test_same_relation_types but it's off as
-# B and C are different on the 16th decimal place
-def test_multiple_relation_types_circular():
-    g = nx.DiGraph()
-    g.add_edges_from([('A', 'B', {'type': 'sector'}),
-                      ('B', 'A', {'type': 'sector'}),
-                      ('A', 'C', {'type': 'sector'}),
-                      ('C', 'A', {'type': 'sector'}),
-                      ('B', 'C', {'type': 'sector'}),
-                      ('C', 'B', {'type': 'sector'}),
-                      ('A', 'B', {'type': 'supplier'}),
-                      ('A', 'C', {'type': 'supplier'})])
-    r = pagerank_edgetypes(g, alpha=1)
-    assert ([k for k, v in sorted(r.iteritems(), key=lambda (k, v): v, reverse=True)] == ['B', 'C', 'A'])
-
-
-
-
-test_multiple_relation_types_circular()
